7c93bdc1df786de14387fe1b861cea826f1d05c1
veggiesaurus <accomrie@gmail.com>
2021-05-07 13:14:23 +0200

    attempt to login on enter key

--- public/dashboard.js
+++ public/dashboard.js
@@ -5,7 +5,7 @@ let redirectUrl;
 let autoRedirect = false;
 redirectUrl = `${strippedPath}`;
 if (urlParams.has("redirectParams")) {
-    redirectUrl+= atob(urlParams.get("redirectParams"));
+    redirectUrl += atob(urlParams.get("redirectParams"));
     autoRedirect = true;
 }
 
@@ -280,6 +280,15 @@ handleLocalLogout = async () => {
     await apiCall("auth/logout", undefined, "post", false);
 }
 
+handleKeyup = (e) => {
+    if (e.keyCode === 13) {
+        const loginButton = document.getElementById("login");
+        if (loginButton && !loginButton.disabled) {
+            handleLogin();
+        }
+    }
+}
+
 refreshGoogleToken = async () => {
     try {
         if (gapi && gapi.auth2) {
@@ -382,4 +391,6 @@ window.onload = async () => {
     document.getElementById("refresh-logs").onclick = handleLog;
     document.getElementById("hide-logs").onclick = handleHideLog;
     document.getElementById("logout").onclick = handleLogout;
+    document.getElementById("username").onkeyup = handleKeyup;
+    document.getElementById("password").onkeyup = handleKeyup;
 }
