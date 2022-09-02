242988a141d47f97d72af8f5786501c235d78e74
veggiesaurus <accomrie@gmail.com>
2022-09-02 12:03:58 +0200

    bumped version and carta-frontend version

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Angus Comrie, Adrianna Pińska and Robert Simmonds'
 author = 'Angus Comrie, Adrianna Pińska and Robert Simmonds'
 
 # The full version, including alpha/beta/rc tags
-release = '3.0.0'
+release = '3.0.1'
 
 
 # -- General configuration ---------------------------------------------------
