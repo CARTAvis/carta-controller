64766f4ad4d7036e259b87b0a19839866a0d9ae2
Angus Comrie <accomrie@gmail.com>
2023-02-27 15:28:37 +0200

    Angus/remove userid package (#137)
    * replaced userid package with wrapper to id command
    
    * fixed typo
    
    * adds minor regex check on username
    
    * switch to spawnSync from execSync
    
    * adds comment on regex
    
    * adds ability to override username regex using NAME_REGEX env variable

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,6 @@
 import * as express from "express";
+import {spawnSync} from "child_process";
+
 import {verboseOutput} from "./config";
 
 // Delay for the specified number of milliseconds
@@ -25,4 +27,36 @@ export function verboseError(...args: any[]) {
     if (verboseOutput) {
         console.error(args);
     }
+}
+
+export function getUserId(username: string) {
+    if (!username) {
+        throw new Error("Missing argument for username");
+    }
+
+    let usernameRegex: RegExp | undefined = undefined;
+    if (process.env.NAME_REGEX) {
+        try {
+            usernameRegex = new RegExp(process.env.NAME_REGEX);
+        } catch (err) {
+            console.warn(err);
+        }
+    }
+
+    if (!usernameRegex) {
+        // As specified in useradd manpage
+        usernameRegex = /^[a-z_][a-z0-9_-]*[$]?$/gm;
+    }
+    if (!username.match(usernameRegex)) {
+        throw new Error("Malformed argument for username");
+    }
+
+    const result = spawnSync("id", ["-u", username])?.stdout;
+    if (result) {
+        const uid = Number.parseInt(result);
+        if (isFinite(uid)) {
+            return uid;
+        }
+    }
+    throw new Error(`Can't find uid for username ${username}`);
 }
