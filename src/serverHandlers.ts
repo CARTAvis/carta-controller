b06b30ebca2e2d7d073253139e617c1ec365de58
crocka <raulomar@ualberta.ca>
2023-08-28 00:51:11 -0400

    Added controller-build flag for telemetry data

--- src/serverHandlers.ts
+++ src/serverHandlers.ts
@@ -192,7 +192,8 @@ async function startServer(username: string) {
             "--port",
             `${port}`,
             "--top_level_folder",
-            ServerConfig.rootFolderTemplate.replace("{username}", username)
+            ServerConfig.rootFolderTemplate.replace("{username}", username),
+            "--controller-build"
         ]);
 
         if (ServerConfig.logFileTemplate) {
