a79bd389190dfc55c99dc235661105683065813a
veggiesaurus <accomrie@gmail.com>
2023-10-09 17:03:52 +0200

    test script for starting a yjs doc

--- scripts/bun_websocket_test.ts
+++ scripts/bun_websocket_test.ts
@@ -1,20 +1,22 @@
-const socket = new WebSocket("ws://localhost:8001/api/collaboration/helloworld?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjYXJ0YSIsInVzZXJuYW1lIjoiYW5ndXMiLCJpYXQiOjE2OTY4NjIwMjUsImV4cCI6MTY5Njg2MjkyNX0.hbRQyZ7-3Jl8ZWt_9C9LPpluTObxi5Nne4jGLYaQ75NCDbC1PVO8XF45gjPk7wo9D7kHuLMu6db3uLFbLXp-WVVF5fOtUnviOX_nHkfrbujjF6_gTju__l5zhciPl_rlV2TDuL64UCaBDtOooDx5cFI00eVud5AJO7zgav3hgADl2_QMw79BWKU53tOgLlTemczQPosyhV34nJGJDm6huWYXW4FI3DsOm6vgx3f9UAoftPTaGMjJjMFTmshPf_IsrHGOLlOskTwVHdyvWmH2zvneHOfoEY2BJoHcae5hUQstrU5NPlH7W6PzNmKSePTC00w6x5TzjsAmPOc4SMTErub2Rt24iIp9HzVkZS8O12ZHBx87w_Pwy1Jhjrqh58mG8oGBFelQiPga-eMg8QgPfx_0DleLlOwgVzRax4g7PgJrUD5xl8GzGePekIEFz-hbvxaUKRWJq8FIuSIScIS8nb2ffO41SDjmyR5wPI5J7u8uyCYmQ2f5ITseQJOjwnhzzYxGsjrow6kzRSt0I1Pb6oBE0g0IJynIlyf9ixWyBHxMcetTxWUS9p-KjLqgdPq1qnaTf6JyXQRZZ7wOE4osRrOb4cYpk9eMv225nlc-HQoOJEUurYStYMkAKLryjovJHUtujqo9DkmKglPt2cFdYHruU_-2xnGcc__A2qHRsPQ");
-// message is received
-socket.addEventListener("message", event => {
-    console.log("Message from server ", event.data);
+import * as Y from "yjs";
+import {WebsocketProvider} from "y-websocket";
+
+const ydoc = new Y.Doc();
+
+// Sync clients with the y-websocket provider
+const token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjYXJ0YSIsInVzZXJuYW1lIjoiYW5ndXMiLCJpYXQiOjE2OTY4NjMzOTksImV4cCI6MTY5Njg2NDI5OX0.ZLzT-DekUyhScBHq-fuo2dn5pNPcubHuxmB9fQov8e0_lUrfOf2q4msrcuLPCbI7crlmGZDc_3QZHcqmznHxYGNEmHXUyH-Ks16uGaVUPRB_GavLLs5eKoc1QAxnY8uGA66bIHz-2VSuKiOuAhPB7_zeuDliTGXQCcGsDEgYRI_dv1Ho0xpqA0Rku4CBQLmqWBvLk6JbS6bCCZf8YHPVyBf8NYDxkaS4LCGrhAJ_pdZ_24AmbrzK_wAmcUC1Gpm7JfgsBBn-pd3uTM5kN5_6RWBGItQq16tiCpYEHSBTHRA4WF8fZddYu_nNgdSH_sphX1JSrABppOctEOrD_nAWIUavhYoyOrICQ2DypKDX6j4tzER18NUtNCeSCSDXPxCXhz2-r-oQ2G4tAOMSxtZkS5bWq6dGoONxYNcVAGyxuggLYeUZhFC30cV-VBWKG1XKva2z4GZux_FesFpfJlOBpjttsR7BI9ceHjVfystYgl6s9FYn3rmiFpFbCGOPwhPMHcx2PzRXqUdQ5mmxFgQT_Y3Z135a5rWQD6N6CNT0XwapMRtKox9g9CT4_l7v2fe3qcHX1L4HjsogWtECvmX7zX_IRXhfcRN2sdLwZSnDiKLW4Y0NbZTc2JOV-Ezb-RHdjuHkhb6yazxQElNJcgopcCh5j8k5XHna8pVk9Fas6VM";
+const websocketProvider = new WebsocketProvider("ws://localhost:8001/api/collaboration", "count-demo", ydoc, {params: {token}});
+
+// array of numbers which produce a sum
+const yarray = ydoc.getArray("count");
+
+// observe changes of the sum
+yarray.observe(event => {
+    // print updates when the data changes
+    console.log("new sum: " + yarray.toArray().reduce((a, b) => a + b));
 });
 
-// socket opened
-socket.addEventListener("open", event => {
-    console.log("Message from server ", event);
-});
+// add 1 to the sum
+yarray.push([1]); // => "new sum: 1"
 
-// socket closed
-socket.addEventListener("close", event => {
-    console.log("Message from server ", event);
-});
 
-// error handler
-socket.addEventListener("error", event => {
-    console.log("Message from server ", event);
-});
