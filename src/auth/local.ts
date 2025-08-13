998ffa2b23c6e01bc343b293efdcf70488725cb7
David Aikema <david.aikema@uct.ac.za>
2025-08-13 12:59:47 +0200

    biome's format --write

--- src/auth/local.ts
+++ src/auth/local.ts
@@ -1,109 +1,143 @@
-import {CartaLocalAuthConfig, ScriptingAccess, Verifier} from "../types";
+import { CartaLocalAuthConfig, ScriptingAccess, Verifier } from "../types";
 import * as fs from "fs";
 import jwt = require("jsonwebtoken");
-import {VerifyOptions} from "jsonwebtoken";
+import { VerifyOptions } from "jsonwebtoken";
 import express from "express";
-import {verifyToken} from "./index";
-import {RuntimeConfig, ServerConfig} from "../config";
+import { verifyToken } from "./index";
+import { RuntimeConfig, ServerConfig } from "../config";
 import ms from "ms";
-import {getUserId} from "../util";
+import { getUserId } from "../util";
 import { logger } from "../util";
 
 let privateKey: Buffer;
 
 export enum TokenType {
-    Access,
-    Refresh,
-    Scripting
+	Access,
+	Refresh,
+	Scripting,
 }
 
-export function generateToken(authConf: CartaLocalAuthConfig, username: string, tokenType: TokenType) {
-    if (!privateKey) {
-        privateKey = fs.readFileSync(authConf.privateKeyLocation);
-    }
-    if (!authConf || !privateKey) {
-        return null;
-    }
+export function generateToken(
+	authConf: CartaLocalAuthConfig,
+	username: string,
+	tokenType: TokenType,
+) {
+	if (!privateKey) {
+		privateKey = fs.readFileSync(authConf.privateKeyLocation);
+	}
+	if (!authConf || !privateKey) {
+		return null;
+	}
 
-    const payload: any = {
-        iss: authConf.issuer,
-        username
-    };
+	const payload: any = {
+		iss: authConf.issuer,
+		username,
+	};
 
-    const options: jwt.SignOptions = {
-        algorithm: authConf.keyAlgorithm,
-        expiresIn: authConf.accessTokenAge
-    };
+	const options: jwt.SignOptions = {
+		algorithm: authConf.keyAlgorithm,
+		expiresIn: authConf.accessTokenAge,
+	};
 
-    if (tokenType === TokenType.Refresh) {
-        payload.refresh = true;
-        options.expiresIn = authConf.refreshTokenAge;
-    } else if (tokenType === TokenType.Scripting) {
-        payload.scripting = true;
-        options.expiresIn = authConf.scriptingTokenAge;
-    }
+	if (tokenType === TokenType.Refresh) {
+		payload.refresh = true;
+		options.expiresIn = authConf.refreshTokenAge;
+	} else if (tokenType === TokenType.Scripting) {
+		payload.scripting = true;
+		options.expiresIn = authConf.scriptingTokenAge;
+	}
 
-    return jwt.sign(payload, privateKey, options);
+	return jwt.sign(payload, privateKey, options);
 }
 
-export function addTokensToResponse(res: express.Response, authConf: CartaLocalAuthConfig, username: string) {
-    const refreshToken = generateToken(authConf, username, TokenType.Refresh);
-    res.cookie("Refresh-Token", refreshToken, {
-        path: RuntimeConfig.authPath,
-        maxAge: ms(authConf.refreshTokenAge as string),
-        httpOnly: true,
-        secure: !ServerConfig.httpOnly,
-        sameSite: "strict"
-    });
+export function addTokensToResponse(
+	res: express.Response,
+	authConf: CartaLocalAuthConfig,
+	username: string,
+) {
+	const refreshToken = generateToken(authConf, username, TokenType.Refresh);
+	res.cookie("Refresh-Token", refreshToken, {
+		path: RuntimeConfig.authPath,
+		maxAge: ms(authConf.refreshTokenAge as string),
+		httpOnly: true,
+		secure: !ServerConfig.httpOnly,
+		sameSite: "strict",
+	});
 
-    const access_token = generateToken(authConf, username, TokenType.Access);
+	const access_token = generateToken(authConf, username, TokenType.Access);
 
-    res.json({
-        access_token,
-        token_type: "bearer",
-        expires_in: ms(authConf.accessTokenAge as string) / 1000
-    });
+	res.json({
+		access_token,
+		token_type: "bearer",
+		expires_in: ms(authConf.accessTokenAge as string) / 1000,
+	});
 }
 
-export function generateLocalVerifier(verifierMap: Map<string, Verifier>, authConf: CartaLocalAuthConfig) {
-    const publicKey = fs.readFileSync(authConf.publicKeyLocation);
-    verifierMap.set(authConf.issuer, cookieString => {
-        const payload: any = jwt.verify(cookieString, publicKey, {algorithm: authConf.keyAlgorithm} as VerifyOptions);
-        if (payload && payload.iss === authConf.issuer) {
-            return payload;
-        } else {
-            return undefined;
-        }
-    });
+export function generateLocalVerifier(
+	verifierMap: Map<string, Verifier>,
+	authConf: CartaLocalAuthConfig,
+) {
+	const publicKey = fs.readFileSync(authConf.publicKeyLocation);
+	verifierMap.set(authConf.issuer, (cookieString) => {
+		const payload: any = jwt.verify(cookieString, publicKey, {
+			algorithm: authConf.keyAlgorithm,
+		} as VerifyOptions);
+		if (payload && payload.iss === authConf.issuer) {
+			return payload;
+		} else {
+			return undefined;
+		}
+	});
 }
 
 export function generateLocalRefreshHandler(authConf: CartaLocalAuthConfig) {
-    return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-        const refreshTokenCookie = req.cookies["Refresh-Token"];
-        const scriptingToken = req.body?.scripting === true;
-        if (refreshTokenCookie) {
-            try {
-                const refreshToken = await verifyToken(refreshTokenCookie);
-                if (!refreshToken || !refreshToken.username || !refreshToken.refresh) {
-                    next({statusCode: 403, message: "Not authorized"});
-                } else if (scriptingToken && ServerConfig.scriptingAccess !== ScriptingAccess.Enabled) {
-                    next({statusCode: 500, message: "Scripting access not enabled for this server"});
-                } else {
-                    const uid = getUserId(refreshToken.username);
-                    const access_token = generateToken(authConf, refreshToken.username, scriptingToken ? TokenType.Scripting : TokenType.Access);
-                    logger.info(`Refreshed ${scriptingToken ? "scripting" : "access"} token for user ${refreshToken.username} with uid ${uid}`);
-                    res.json({
-                        access_token,
-                        token_type: "bearer",
-                        username: refreshToken.username,
-                        expires_in: ms(scriptingToken ? authConf.scriptingTokenAge : (authConf.accessTokenAge as string)) / 1000
-                    });
-                }
-            } catch (err) {
-                next({statusCode: 400, message: "Invalid refresh token"});
-            }
-        } else {
-            next({statusCode: 400, message: "Missing refresh token"});
-        }
-    };
+	return async (
+		req: express.Request,
+		res: express.Response,
+		next: express.NextFunction,
+	) => {
+		const refreshTokenCookie = req.cookies["Refresh-Token"];
+		const scriptingToken = req.body?.scripting === true;
+		if (refreshTokenCookie) {
+			try {
+				const refreshToken = await verifyToken(refreshTokenCookie);
+				if (!refreshToken || !refreshToken.username || !refreshToken.refresh) {
+					next({ statusCode: 403, message: "Not authorized" });
+				} else if (
+					scriptingToken &&
+					ServerConfig.scriptingAccess !== ScriptingAccess.Enabled
+				) {
+					next({
+						statusCode: 500,
+						message: "Scripting access not enabled for this server",
+					});
+				} else {
+					const uid = getUserId(refreshToken.username);
+					const access_token = generateToken(
+						authConf,
+						refreshToken.username,
+						scriptingToken ? TokenType.Scripting : TokenType.Access,
+					);
+					logger.info(
+						`Refreshed ${scriptingToken ? "scripting" : "access"} token for user ${refreshToken.username} with uid ${uid}`,
+					);
+					res.json({
+						access_token,
+						token_type: "bearer",
+						username: refreshToken.username,
+						expires_in:
+							ms(
+								scriptingToken
+									? authConf.scriptingTokenAge
+									: (authConf.accessTokenAge as string),
+							) / 1000,
+					});
+				}
+			} catch (err) {
+				next({ statusCode: 400, message: "Invalid refresh token" });
+			}
+		} else {
+			next({ statusCode: 400, message: "Missing refresh token" });
+		}
+	};
 }
--- src/auth/pam.ts
+++ src/auth/pam.ts
@@ -1,32 +1,46 @@
-import {Request, Response} from "express";
-import {CartaLocalAuthConfig} from "../types";
-import {addTokensToResponse} from "./local";
-import {getUserId} from "../util";
+import { Request, Response } from "express";
+import { CartaLocalAuthConfig } from "../types";
+import { addTokensToResponse } from "./local";
+import { getUserId } from "../util";
 import { logger } from "../util";
 
 export function getPamLoginHandler(authConf: CartaLocalAuthConfig) {
-    const {pamAuthenticate} = require("node-linux-pam");
+	const { pamAuthenticate } = require("node-linux-pam");
 
-    return (req: Request, res: Response) => {
-        let username = req.body?.username;
-        const password = req.body?.password;
+	return (req: Request, res: Response) => {
+		let username = req.body?.username;
+		const password = req.body?.password;
 
-        if (!username || !password) {
-            return res.status(400).json({statusCode: 400, message: "Malformed login request"});
-        }
+		if (!username || !password) {
+			return res
+				.status(400)
+				.json({ statusCode: 400, message: "Malformed login request" });
+		}
 
-        pamAuthenticate({username, password}, (err: Error | string, code: number) => {
-            if (err) {
-                return res.status(403).json({statusCode: 403, message: "Invalid username/password combo"});
-            } else {
-                try {
-                    const uid = getUserId(username);
-                    logger.info(`Authenticated as user ${username} with uid ${uid} using PAM`);
-                    return addTokensToResponse(res, authConf, username);
-                } catch (e) {
-                    return res.status(403).json({statusCode: 403, message: "User does not exist"});
-                }
-            }
-        });
-    };
+		pamAuthenticate(
+			{ username, password },
+			(err: Error | string, code: number) => {
+				if (err) {
+					return res
+						.status(403)
+						.json({
+							statusCode: 403,
+							message: "Invalid username/password combo",
+						});
+				} else {
+					try {
+						const uid = getUserId(username);
+						logger.info(
+							`Authenticated as user ${username} with uid ${uid} using PAM`,
+						);
+						return addTokensToResponse(res, authConf, username);
+					} catch (e) {
+						return res
+							.status(403)
+							.json({ statusCode: 403, message: "User does not exist" });
+					}
+				}
+			},
+		);
+	};
 }
